c142e3088438a14a3c0f7c48efd286564e4001b8
Nicolas <nicolas.bernal-f@mail.escuelaing.edu.co>
2024-02-18 16:44:56 -0500

    Final

--- src/main/java/edu/eci/cvds/tdd/registry/Registry.java
+++ src/main/java/edu/eci/cvds/tdd/registry/Registry.java
@@ -1,18 +1,23 @@
 package edu.eci.cvds.tdd.registry;
 
+import java.util.ArrayList;
+
 public class Registry {
+
+    public static final int MAX_AGE = 135;
+    public static final int VOTING_AGE = 18;
+    private ArrayList<Integer> voters = new ArrayList<Integer>();
+
     public RegisterResult registerVoter(Person p) {
-        if (p.getAge() < 18 && p.getAge() >= 0){
-            return RegisterResult.UNDERAGE;
+        RegisterResult result = null;
+        if (!p.isAlive()) result =  RegisterResult.DEAD;
+        if (0 <= p.getAge() && p.getAge() < VOTING_AGE) result =  RegisterResult.UNDERAGE;
+        if (p.getAge() < 0 || p.getAge() > MAX_AGE) result =  RegisterResult.INVALID_AGE;
+        if (voters.contains(p.getId())) result = RegisterResult.DUPLICATED;
+        if (result == null) {
+            voters.add(p.getId());
+            result = RegisterResult.VALID;
         }
-        if (p.isAlive() == false){
-            return RegisterResult.DEAD;
-        }
-        if (p.getAge() <= -1 || p.getAge() > 135){
-            return RegisterResult.INVALID_AGE;
-        }
-        else {
-            return RegisterResult.VALID;
-        }
+        return result;
     }
 }
